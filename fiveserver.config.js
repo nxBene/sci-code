f098fa61ed66359fe860636c6d8ab3b23d26646c
nxBene <knoepfleB@outlook.de>
2024-10-16 22:56:24 +0200

    Add amplify intigration

---
+++ fiveserver.config.js
@@ -0,0 +1,8 @@
+// fiveserver.config.js
+module.exports = {
+  injectBody: true, // enable instant update
+  navigate: true, // enable auto-navigation
+  debugVSCode: true,
+  php: "/opt/homebrew/bin/php",
+  browser: "firefox developer edition",
+};
